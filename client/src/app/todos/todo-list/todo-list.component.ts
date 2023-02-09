2e218c7abccdecdbf8adc9d6e8a40ff32888e2c7
Mason Eischens <eisch115@morris.umn.edu>
2023-02-09 14:13:20 -0600

    Adds functions to todo-list.component
    Co-authored-by: Brendan Conroy <@brendanconroy9>

--- client/src/app/todos/todo-list/todo-list.component.ts
+++ client/src/app/todos/todo-list/todo-list.component.ts
@@ -7,7 +7,7 @@ import { TodoService } from '../todo.service';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   public serverFilteredTodos: Todo[];
   public filteredTodos: Todo[];
 
@@ -17,4 +17,33 @@ export class TodoListComponent {
   public todoOwner: string;
   public todoCategory: string;
   public viewType: 'card' | 'list' = 'card';
+
+  constructor(private todoService: TodoService, private snackBar: MatSnackBar){
+
+  }
+
+  getTodosFromServer() {
+    this.todoService.getTodos({
+
+    }).subscribe(returnedTodos => {
+      this.serverFilteredTodos = returnedTodos;
+      this.updateFilter();
+    }, err => {
+      console.error('We couldn\'t get the list of todos; the server might be down :(');
+      this.snackBar.open(
+        'Problem contacing server – try again',
+        'OK',
+        { duration: 3000});
+    });
+  }
+
+  public updateFilter() {
+    this.filteredTodos = this.todoService.filterTodos(
+      this.serverFilteredTodos, { owner: this.todoOwner, category: this.todoCategory, status: this.todoStatus }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
 }
